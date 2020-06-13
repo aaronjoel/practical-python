1cc7f3decb6930507348c4b9b6230f57d82c0ad1
aaronjoel <jlontsi@gmail.com>
2020-06-13 12:21:55 +0200

    modified pcost.py to make the code reusable by using a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,28 +2,25 @@
 #
 # Exercise 1.27
 
-# open portfolio file for reading
-f = open('Data/portfolio.csv', 'rt')
-# skip the header line
-headers = next(f)
+def portfolio_cost(filename):
+    """
+    Returns the total cost for all shares from 'filename'
+    """
+    total_cost = 0
+    with open(filename) as fp:
+        for line in fp:
+            stock_name, stock_num, stock_price = line.strip().split(',')
+            try:
+                stock_num = int(stock_num)
+            except ValueError:
+                stock_num = 0
+            try:
+                stock_price = float(stock_price)
+            except ValueError:
+                stock_price = 0.0
+            total_cost += stock_num * stock_price
+        return total_cost
 
-# hold the total cost
-total_cost = 0
-
-# iterate over the file to compute the total cost for the shares purchases
-for line in f:
-    stock_name, stock_num, stock_price = line.strip().split(',')
-    try:
-        stock_num = int(stock_num)
-    except Exception:
-        stock_num = 0
-    try:
-        stock_price = float(stock_price)
-    except:
-        stock_price = 0
-    total_cost += stock_num * stock_price
-
-# close the file
-f.close()
-
-print(f"The total cost for all the shares is {total_cost:.2f}")
+if __name__ == '__main__':
+    cost = portfolio_cost('Data/portfolio.csv')
+    print(f"The total cost for all the shares is {cost:.2f}")
