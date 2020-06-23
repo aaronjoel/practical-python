bb09ddffcb090caed5d67ae4c3df0caa8fbe898d
aaronjoel <jlontsi@gmail.com>
2020-06-23 23:31:02 +0200

    modified fileparse.py to work for non csv files

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,11 +3,11 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename: str, select=None, types=None, has_headers: bool=False) -> list:
+def parse_csv(filename: str, select=None, types=None, has_headers: bool=False, delimiter: str=' ') -> list:
     '''Parse a CSV file into a list of records'''
 
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers
         if has_headers:
