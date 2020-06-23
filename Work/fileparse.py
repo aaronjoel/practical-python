507884d48425196a618d4a7ee70fed31416484a8
aaronjoel <jlontsi@gmail.com>
2020-06-23 23:12:47 +0200

    modified fileparse.py to make use of default parameter

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename: str) -> list:
+def parse_csv(filename: str, select=None) -> list:
     '''Parse a CSV file into a list of records'''
 
     with open(filename) as f:
@@ -11,10 +11,24 @@ def parse_csv(filename: str) -> list:
 
         # Read the file headers
         headers = next(rows)
+
+        # If a column selector was given, find indices of the specified columns.
+        # Also narrow the set of headers userd for resulting dictionaries
+        if select:
+            indices = [headers.index(colname) for colname if select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:   # Skip rows with no data
                 continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+
+            # Make a dictionary
             record = dict(zip(headers, row))
             records.append(record)
  
