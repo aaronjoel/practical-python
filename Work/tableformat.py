c71261f6444ce97350080d6df72d22ffd13c935d
aaronjoel <jlontsi@gmail.com>
2020-06-27 19:56:56 +0200

    modified tableformat.py to include a concrete TextTableFormatter class that implements the abstract class TableFormatter

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -13,3 +13,17 @@ class TableFormatter:
         '''
         raise NotImplementedError()
 
+class TextTableFormatter(TableFormatter):
+    '''
+    Emit a table in plain-text format
+    '''
+    def headings(self, headers):
+        for h in headers:
+            print(f'{h:>10s}', end=' ')
+        print()
+        print(('-' * 10 + ' ') * len(headers))
+
+    def row(self, rowdata):
+        for d in rowdata:
+            print(f'{d:>10s}', end=' ')
+        print()
