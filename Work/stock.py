98d002bc7e71799d969a0f6e10e5ac8d5de57a4d
aaronjoel <jlontsi@gmail.com>
2020-07-21 03:56:55 +0200

    modified the Stock class from stock.py to include __repr__

--- Work/stock.py
+++ Work/stock.py
@@ -7,6 +7,9 @@ class Stock:
     
     def cost(self) -> float:
         return self.shares * self.price
+
+    def __repr__(self) -> str:
+        return f'Stock({self.name}, {self.shares}, {self.price})'
     
     def sell(self, nshares) -> None:
         left = self.shares - nshares
