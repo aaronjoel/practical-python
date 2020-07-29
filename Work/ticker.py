98a0baf3302d688399da4cfbd624278081ad308e
aaronjoel <jlontsi@gmail.com>
2020-07-29 02:13:55 +0200

    ticker.py/select_columns a new function that selects specific columns

--- Work/ticker.py
+++ Work/ticker.py
@@ -5,8 +5,12 @@ import csv
 
 def parse_stock_data(lines):
     rows = csv.reader(lines)
+    rows = select_columns(rows, [0, 1, 4])
     return rows
 
+def select_columns(rows, indices):
+    for row in rows:
+        yield [row[index] for index in indices]
 
 if __name__ == '__main__':
     lines = follow('Data/stocklog.csv')
